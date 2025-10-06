a1ee61f5d92c41ef583ba494a8f219dc16a529c3
miladmo68 <miladmo68@gmail.com>
2025-10-06 17:49:38 -0400

    Updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -76,7 +76,8 @@ export default function Hero({ onOpenLightbox }) {
         {!fallback ? (
           <video
             ref={videoRef}
-            className="absolute inset-0 h-full w-full object-cover"
+            className="absolute inset-0 h-full w-full object-cover [object-position:30%_50%]     
+            sm:[object-position:center] "
             src="/assets/video/hero.mp4"
             autoPlay
             muted
