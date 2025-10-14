f820f11dfd34ac7142e4815f0f572c354b2d9c81
miladmo68 <miladmo68@gmail.com>
2025-10-14 11:41:10 -0400

    Contact Added

--- api/contact.js
+++ api/contact.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT || 465),
-      secure: true, // 465 → SSL
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
